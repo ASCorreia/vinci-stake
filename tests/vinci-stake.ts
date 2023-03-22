4ec40d8c59c29d44fed6add41b75fd09dcebb563
André Correia <ascorreia@criticalsoftware.com>
2023-03-22 01:10:02 +0000

    Used some black magic to make this "work"

--- tests/vinci-stake.ts
+++ tests/vinci-stake.ts
@@ -4,13 +4,25 @@ import { VinciStake } from "../target/types/vinci_stake";
 
 describe("vinci-stake", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const key = anchor.AnchorProvider.env()
+  anchor.setProvider(key);
 
   const program = anchor.workspace.VinciStake as Program<VinciStake>;
 
   it("Is initialized!", async () => {
+    const [vinciWorldStake, _] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode("VinciWorldStakePool"),
+        key.wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+      );
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initializeStakePool().accounts({
+      stakePool: vinciWorldStake,
+      user: key.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).rpc();
     console.log("Your transaction signature", tx);
   });
 });
